03dbb399379ab5b3f805f6baedea8cde2da88370
NikhilUSharma28 <77388716+NikhilUSharma28@users.noreply.github.com>
2021-01-18 12:52:52 +0530

    Update CreateListPDF.java

--- src/CreateListPDF.java
+++ src/CreateListPDF.java
@@ -1,63 +1,48 @@
 import java.io.IOException;
 import java.util.List;
-
 import javax.servlet.http.HttpServletResponse;
-
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.*;
 
-public class CreateListPDF {
+public class DownloadList {
 
 	private List<Inventory> inven_var;
 	
-	public CreateListPDF(List<Inventory> inven_var) {
+	public DownloadList(List<Inventory> inven_var) {
 		super();
 		this.inven_var = inven_var;
 	}
 
-	private void writeTableHeader(PdfPTable table) {
+	private void tableHeader(PdfPTable table) {
         PdfPCell cell = new PdfPCell();
-        cell.setPadding(3);
-
-        Font font = FontFactory.getFont(FontFactory.HELVETICA);
-
-        cell.setPhrase(new Phrase("Name", font));
+        cell.setPhrase(new Phrase("Name"));
         table.addCell(cell);
-
-        cell.setPhrase(new Phrase("Rating", font));
+        cell.setPhrase(new Phrase("Rating"));
         table.addCell(cell);
 
     }
 	
-	private void writeTableData(PdfPTable table) {
+	private void tableData(PdfPTable table) {
 		for(Inventory inventory:inven_var) {
-			
 			table.addCell(inventory.getName());
 	        table.addCell(String.valueOf(inventory.getRating()));
 		}
     }
 	
-	public void export(HttpServletResponse response) throws DocumentException, IOException {
+	public void downloadList(HttpServletResponse response) throws DocumentException, IOException {
 		com.itextpdf.text.Document document = new com.itextpdf.text.Document(PageSize.A4.rotate());
         PdfWriter.getInstance(document, response.getOutputStream());
 
         document.open();
-
-
         Paragraph p = new Paragraph("Product List");
-
-
         document.add(p);
-
         PdfPTable table = new PdfPTable(2);
         table.setWidthPercentage(100f);
         table.setSpacingBefore(10);
 
-        writeTableHeader(table);
-        writeTableData(table);
-
+        tableHeader(table);
+        tableData(table);
         document.add(table);
-
         document.close();
     }
 }
